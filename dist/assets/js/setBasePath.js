72d58a2d46e5cb85b6a88c857af700f77caf54b9
Martina Kessler <martina.kessler@hispeed.ch>
2025-05-23 15:52:32 +0200

    remove dist folder

--- dist/assets/js/setBasePath.js
+++
@@ -1,4 +0,0 @@
-// setBasePath.js
-const isGitHubPages = location.hostname.includes('github.io');
-const basePath = isGitHubPages ? '/<repository>/' : '/';
-document.querySelector('base').setAttribute('href', basePath);
